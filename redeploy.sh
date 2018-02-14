59789f37fa72006c9d910af98c3a7e953c6291fd
halil.ozkan <halil.ozkan@verapi.com>
2018-02-14 12:42:34 +0300

    [+] 400 html page added [*] 401, 403 and 404 pages altered [+] redeploy.bat/sh added

---
+++ redeploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export LAUNCHER="com.verapi.starter.PortalLauncher"
+export VERTICLE="com.verapi.starter.InitVerticle"
+export CMD="mvn compile"
+export VERTX_CMD="run"
+
+mvn compile dependency:copy-dependencies
+java \
+  -cp  $(echo target/dependency/*.jar | tr ' ' ':'):"target/classes" \
+  $LAUNCHER $VERTX_CMD $VERTICLE \
+  --redeploy="src/main/**/*" --on-redeploy="$CMD" \
+  --launcher-class=$LAUNCHER \
+  $@
