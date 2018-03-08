a9429288eb66633fe0f90e21023ea92003ea809a
faik.saglar <faik.saglar@verapi.com>
2018-03-08 20:35:48 +0300

    [*] JdbcClient is get from service discovery [*] Router configuration is moved inside handler

--- src/main/java/com/verapi/portal/MainVerticle.java
+++ src/main/java/com/verapi/portal/MainVerticle.java
@@ -71,116 +71,129 @@ public class MainVerticle extends AbstractVerticle {
             }
         });
 
-        JsonObject jdbcConfig = new JsonObject().put("url", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_JDBC_URL))
-        .put("driver_class", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_JDBC_DRIVER_CLASS))
-        .put("user", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_DBUSER_NAME))
-        .put("password", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_DBUSER_PASSWORD))
-        .put("max_pool_size", Config.getInstance().getConfigJsonObject().getInteger(Constants.PORTAL_DBCONN_MAX_POOL_SIZE));
+//        JsonObject jdbcConfig = new JsonObject().put("url", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_JDBC_URL))
+//        .put("driver_class", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_JDBC_DRIVER_CLASS))
+//        .put("user", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_DBUSER_NAME))
+//        .put("password", Config.getInstance().getConfigJsonObject().getString(Constants.PORTAL_DBUSER_PASSWORD))
+//        .put("max_pool_size", Config.getInstance().getConfigJsonObject().getInteger(Constants.PORTAL_DBCONN_MAX_POOL_SIZE));
+//        
+//        JDBCClient jdbcClient = JDBCClient.createShared(vertx, jdbcConfig, Constants.PORTAL_DATA_SOURCE_SERVICE);
+//        logger.info("JDBCClient created... " + jdbcClient.toString());
         
-        JDBCClient jdbcClient = JDBCClient.createShared(vertx, jdbcConfig, Constants.PORTAL_DATA_SOURCE_SERVICE);
-        logger.info("JDBCClient created... " + jdbcClient.toString());
-
-        auth = JDBCAuth.create(vertx, jdbcClient);
-
-        logger.info("JDBCAuthProvider created... " + auth.toString());
-
-        auth.setAuthenticationQuery("SELECT PASSWORD, PASSWORD_SALT FROM portalschema.USER WHERE USERNAME = ?");
-        auth.setPermissionsQuery("SELECT PERM FROM portalschema.ROLES_PERMS RP, portalschema.USER_ROLES UR WHERE UR.USERNAME = ? AND UR.ROLE = RP.ROLE");
-        auth.setRolesQuery("SELECT ROLE FROM portalschema.USER_ROLES WHERE USERNAME = ?");
-        auth.setHashStrategy(JDBCHashStrategy.createPBKDF2(vertx));
-        //TODO: authProvider.setNonces();
-        logger.info("JDBCAuthProvider configuration done... ");
-
-
-        // To simplify the development of the web components we use a Router to route all HTTP requests
-        // to organize our code in a reusable way.
-        Router router = Router.router(vertx);
-
-        //log HTTP requests
-        router.route().handler(LoggerHandler.create(LoggerFormat.DEFAULT));
-
-        //firstly install cookie handler
-        //A handler which decodes cookies from the request, makes them available in the RoutingContext and writes them back in the response
-        router.route().handler(CookieHandler.create());
-
-        //secondly install body handler
-        //A handler which gathers the entire request body and sets it on the RoutingContext
-        //It also handles HTTP file uploads and can be used to limit body sizes
-        router.route().handler(BodyHandler.create());
-
-        //thirdly install session handler
-        //A handler that maintains a Session for each browser session
-        //The session is available on the routing context with RoutingContext.session()
-        //The session handler requires a CookieHandler to be on the routing chain before it
-        router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx, "abyss.session")).setSessionCookieName("abyss.session"));
-
-        //This handler should be used if you want to store the User object in the Session so it's available between different requests, without you having re-authenticate each time
-        //It requires that the session handler is already present on previous matching routes
-        //It requires an Auth provider so, if the user is deserialized from a clustered session it knows which Auth provider to associate the session with.
-        router.route().handler(UserSessionHandler.create(auth));
-
-        //An auth handler that's used to handle auth (provided by Shiro Auth prodiver) by redirecting user to a custom login page
-        AuthHandler authHandler = RedirectAuthHandler.create(auth, "/full-width-light/login");
-
-        router.get("/create_user").handler(this::createUser).failureHandler(this::failureHandler);
         
-        Signup signup = new Signup(auth, jdbcClient);
-        router.get("/full-width-light/signup").handler(signup::pageRender).failureHandler(this::failureHandler);
-        router.post("/sign-up").handler(signup).failureHandler(this::failureHandler);
-        
-        //install authHandler for all routes where authentication is required
-        //router.route("/full-width-light/").handler(authHandler);
-        router.route("/full-width-light/index").handler(authHandler.addAuthority("okumaz")).failureHandler(this::failureHandler);
+        JDBCDataSource.getJDBCClient(AbyssServiceDiscovery.getInstance(vertx).getServiceDiscovery(), new JsonObject().put("name", Constants.PORTAL_DATA_SOURCE_SERVICE), resultHandler -> {
+        	
+        	if (resultHandler.succeeded()) {
+	        	JDBCClient jdbcClient = resultHandler.result();
+	        	logger.info("JDBCClient created... " + jdbcClient.toString());
+	        	
+	        	auth = JDBCAuth.create(vertx, jdbcClient);
+	        	logger.info("JDBCAuthProvider created... " + auth.toString());
+	        	
+	            auth.setAuthenticationQuery("SELECT PASSWORD, PASSWORD_SALT FROM portalschema.USER WHERE USERNAME = ?");
+	            auth.setPermissionsQuery("SELECT PERM FROM portalschema.ROLES_PERMS RP, portalschema.USER_ROLES UR WHERE UR.USERNAME = ? AND UR.ROLE = RP.ROLE");
+	            auth.setRolesQuery("SELECT ROLE FROM portalschema.USER_ROLES WHERE USERNAME = ?");
+	            auth.setHashStrategy(JDBCHashStrategy.createPBKDF2(vertx));
+	            //TODO: authProvider.setNonces();
+	            logger.info("JDBCAuthProvider configuration done... ");
+	        	
+	            // To simplify the development of the web components we use a Router to route all HTTP requests
+	            // to organize our code in a reusable way.
+	            Router router = Router.router(vertx);
 
-        // Entry point to the application, this will render a custom Thymeleaf template
-        //router.get("/full-width-light/login").handler(this::loginHandler);
-        Login login = new Login(auth);
-        router.get("/full-width-light/login").handler(login::pageRender).failureHandler(this::failureHandler);
-        router.post("/login-auth").handler(login).failureHandler(this::failureHandler);
+	            //log HTTP requests
+	            router.route().handler(LoggerHandler.create(LoggerFormat.DEFAULT));
 
-        Index index = new Index(auth);
-        router.get("/full-width-light/index").handler(index::pageRender).failureHandler(this::failureHandler);
-        //router.post("/login-auth").handler(new SpecialLoginHandler(auth));
+	            //firstly install cookie handler
+	            //A handler which decodes cookies from the request, makes them available in the RoutingContext and writes them back in the response
+	            router.route().handler(CookieHandler.create());
 
-        //router.post("/login-auth2").handler(FormLoginHandler.create(auth));
+	            //secondly install body handler
+	            //A handler which gathers the entire request body and sets it on the RoutingContext
+	            //It also handles HTTP file uploads and can be used to limit body sizes
+	            router.route().handler(BodyHandler.create());
+
+	            //thirdly install session handler
+	            //A handler that maintains a Session for each browser session
+	            //The session is available on the routing context with RoutingContext.session()
+	            //The session handler requires a CookieHandler to be on the routing chain before it
+	            router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx, "abyss.session")).setSessionCookieName("abyss.session"));
+
+	            //This handler should be used if you want to store the User object in the Session so it's available between different requests, without you having re-authenticate each time
+	            //It requires that the session handler is already present on previous matching routes
+	            //It requires an Auth provider so, if the user is deserialized from a clustered session it knows which Auth provider to associate the session with.
+	            router.route().handler(UserSessionHandler.create(auth));
+
+	            //An auth handler that's used to handle auth (provided by Shiro Auth prodiver) by redirecting user to a custom login page
+	            AuthHandler authHandler = RedirectAuthHandler.create(auth, "/full-width-light/login");
+
+	            router.get("/create_user").handler(this::createUser).failureHandler(this::failureHandler);
+	            
+	            Signup signup = new Signup(auth, jdbcClient);
+	            router.get("/full-width-light/signup").handler(signup::pageRender).failureHandler(this::failureHandler);
+	            router.post("/sign-up").handler(signup).failureHandler(this::failureHandler);
+	            
+	            //install authHandler for all routes where authentication is required
+	            //router.route("/full-width-light/").handler(authHandler);
+	            router.route("/full-width-light/index").handler(authHandler.addAuthority("okumaz")).failureHandler(this::failureHandler);
+
+	            // Entry point to the application, this will render a custom Thymeleaf template
+	            //router.get("/full-width-light/login").handler(this::loginHandler);
+	            Login login = new Login(auth);
+	            router.get("/full-width-light/login").handler(login::pageRender).failureHandler(this::failureHandler);
+	            router.post("/login-auth").handler(login).failureHandler(this::failureHandler);
+
+	            Index index = new Index(auth);
+	            router.get("/full-width-light/index").handler(index::pageRender).failureHandler(this::failureHandler);
+	            //router.post("/login-auth").handler(new SpecialLoginHandler(auth));
+
+	            //router.post("/login-auth2").handler(FormLoginHandler.create(auth));
 
 
-        router.get("/img/*").handler(StaticHandler.create("./img").setWebRoot("webroot/img"));
-        router.get("/vendors/*").handler(StaticHandler.create("/vendors").setWebRoot("webroot/vendors"));
-        router.get("/full-width-light/dist/*").handler(StaticHandler.create("/full-width-light/dist").setWebRoot("webroot/full-width-light/dist"));
+	            router.get("/img/*").handler(StaticHandler.create("./img").setWebRoot("webroot/img"));
+	            router.get("/vendors/*").handler(StaticHandler.create("/vendors").setWebRoot("webroot/vendors"));
+	            router.get("/full-width-light/dist/*").handler(StaticHandler.create("/full-width-light/dist").setWebRoot("webroot/full-width-light/dist"));
 
-        router.routeWithRegex("^/full-width-light/[4|5][0|1]\\d$").handler(this::pGenericHttpStatusCodeHandler).failureHandler(this::failureHandler);
+	            router.routeWithRegex("^/full-width-light/[4|5][0|1]\\d$").handler(this::pGenericHttpStatusCodeHandler).failureHandler(this::failureHandler);
 
-        router.get("/full-width-light/httperror").handler(this::pGenericHttpStatusCodeHandler).failureHandler(this::failureHandler);
+	            router.get("/full-width-light/httperror").handler(this::pGenericHttpStatusCodeHandler).failureHandler(this::failureHandler);
 
-        //only rendering page routings' failures shall be handled by using regex
-        //The regex below will match any string, or line without a line break, not containing the (sub)string '.'
-        router.routeWithRegex("^((?!\\.).)*$").failureHandler(this::failureHandler);
+	            //only rendering page routings' failures shall be handled by using regex
+	            //The regex below will match any string, or line without a line break, not containing the (sub)string '.'
+	            router.routeWithRegex("^((?!\\.).)*$").failureHandler(this::failureHandler);
 
-        router.route().handler(ctx -> {
-            logger.info("router.route().handler invoked... the last bus stop, no any bus stop more, so it is firing 404 now...!.");
-            ctx.fail(404);
+	            router.route().handler(ctx -> {
+	                logger.info("router.route().handler invoked... the last bus stop, no any bus stop more, so it is firing 404 now...!.");
+	                ctx.fail(404);
+	            });
+
+	            logger.info("starting http server");
+	            HttpServerOptions httpServerOptions = new HttpServerOptions();
+
+	            logger.warn("http server is running in plaintext mode. Enable SSL in config for production deployments.");
+	            vertx.createHttpServer(httpServerOptions.setCompressionSupported(true))
+	                    .requestHandler(router::accept)
+	                    .listen(Config.getInstance().getConfigJsonObject().getInteger("port")
+	                            , Config.getInstance().getConfigJsonObject().getString("host")
+	                            , result -> {
+	                                if (result.succeeded()) {
+	                                    logger.info("http server started..." + result.succeeded());
+	                                    start.complete();
+	                                } else {
+	                                    logger.error("http server starting failed..." + result.cause());
+	                                    start.fail(result.cause());
+	                                }
+	                            });
+
+	            logger.debug("loaded config : " + Config.getInstance().getConfigJsonObject().encodePrettily());
+	            
+        	} else {
+                logger.error("serviceDiscovery.getJDBCClient failed..." + resultHandler.cause());
+                start.fail(resultHandler.cause());
+        	}
         });
+        
 
-        logger.info("starting http server");
-        HttpServerOptions httpServerOptions = new HttpServerOptions();
-
-        logger.warn("http server is running in plaintext mode. Enable SSL in config for production deployments.");
-        vertx.createHttpServer(httpServerOptions.setCompressionSupported(true))
-                .requestHandler(router::accept)
-                .listen(Config.getInstance().getConfigJsonObject().getInteger("port")
-                        , Config.getInstance().getConfigJsonObject().getString("host")
-                        , result -> {
-                            if (result.succeeded()) {
-                                logger.info("http server started..." + result.succeeded());
-                                start.complete();
-                            } else {
-                                logger.error("http server starting failed..." + result.cause());
-                                start.fail(result.cause());
-                            }
-                        });
-
-        logger.debug("loaded config : " + Config.getInstance().getConfigJsonObject().encodePrettily());
 
 
     }
