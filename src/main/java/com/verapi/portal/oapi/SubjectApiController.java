1101aeffc80e740f2f2a7d8a1dd1542cb2601691
halil.ozkan <halil.ozkan@verapi.com>
2018-05-17 22:08:33 +0300

    [*] critical bugfix inside request parameters getter api

--- src/main/java/com/verapi/portal/oapi/SubjectApiController.java
+++ src/main/java/com/verapi/portal/oapi/SubjectApiController.java
@@ -16,6 +16,8 @@ import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
+//import io.vertx.reactivex.ext.web.api.RequestParameters;
+import io.vertx.ext.web.api.RequestParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,7 +65,79 @@ public class SubjectApiController extends AbstractApiController {
 
     @AbyssApiOperationHandler
     public void addSubjects(RoutingContext routingContext) {
-        logger.info("addSubjects() invoked");
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        logger.info(methodName + " invoked");
+
+            // Get the parsed parameters
+            RequestParameters params = routingContext.get("parsedParameters");
+
+            // We get an user JSON object validated by Vert.x Open API validator
+            JsonObject subjects = params.body().getJsonObject();
+
+/*
+        JsonObject subjects = new JsonObject().put("path", "addSubjects invoked");
+        if (routingContext.user() != null) {
+            subjects
+                    .put("user", routingContext.user().toString())
+                    .put("user.principal", routingContext.user().principal().toString());
+        }
+*/
+
+
+/*
+        if (1 == 1) {
+            throwApiException(routingContext, BadRequest400Exception.class, "test exception", "very detailed message");
+            return;
+        }
+*/
+
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .setStatusCode(200)
+                .end(subjects.toString(), "UTF-8");
+
+    }
+
+    @AbyssApiOperationHandler
+    public void updateSubjects(RoutingContext routingContext) {
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        logger.info(methodName + " invoked");
+
+        // Get the parsed parameters
+        //RequestParameters params = routingContext.get("parsedParameters");
+
+        // We get an user JSON object validated by Vert.x Open API validator
+        //JsonObject subjects = params.body().getJsonObject();
+
+        JsonObject subjects = new JsonObject().put("path", "addSubjects invoked");
+        if (routingContext.user() != null) {
+            subjects
+                    .put("user", routingContext.user().toString())
+                    .put("user.principal", routingContext.user().principal().toString());
+            //.put("method", getClass().getEnclosingMethod().getName());
+
+        }
+/*
+        if (1 == 1) {
+            throwApiException(routingContext, BadRequest400Exception.class, "test exception", "very detailed message");
+            return;
+        }
+*/
+
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .setStatusCode(200)
+                .end(subjects.toString(), "UTF-8");
+
+    }
+
+    @AbyssApiOperationHandler
+    public void deleteSubjects(RoutingContext routingContext) {
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        logger.info(methodName + " invoked");
 
         // Get the parsed parameters
         //RequestParameters params = routingContext.get("parsedParameters");
