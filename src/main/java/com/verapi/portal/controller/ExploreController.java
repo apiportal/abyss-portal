cc221eebbb7b65e9f6cd748000c000b08a368680
halil.ozkan <halil.ozkan@verapi.com>
2018-06-25 23:45:05 +0300

    [+] explore controller developed

---
+++ src/main/java/com/verapi/portal/controller/ExploreController.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 6 2018
+ *
+ */
+
+package com.verapi.portal.controller;
+
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AbyssController(routePathGET = "explore", routePathPOST = "", htmlTemplateFile = "explore.html")
+public class ExploreController extends PortalAbstractController {
+    private static Logger logger = LoggerFactory.getLogger(ExploreController.class);
+
+    public ExploreController(JDBCAuth authProvider, JDBCClient jdbcClient) {
+        super(authProvider, jdbcClient);
+    }
+
+    @Override
+    public void defaultGetHandler(RoutingContext routingContext) {
+        logger.trace("ExploreController.defaultGetHandler invoked...");
+        renderTemplate(routingContext, getClass().getAnnotation(AbyssController.class).htmlTemplateFile());
+    }
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        logger.trace("ExploreController.handle invoked...");
+    }
+
+}
