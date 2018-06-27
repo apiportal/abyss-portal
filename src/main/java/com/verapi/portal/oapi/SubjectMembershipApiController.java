6d5f5f3147b48eab5f0435560d4a4252b7ba4010
halil.ozkan <halil.ozkan@verapi.com>
2018-06-27 17:23:21 +0300

    [+] /subject-memberships/subject/{uuid} api path get method developed

--- src/main/java/com/verapi/portal/oapi/SubjectMembershipApiController.java
+++ src/main/java/com/verapi/portal/oapi/SubjectMembershipApiController.java
@@ -11,8 +11,8 @@
 
 package com.verapi.portal.oapi;
 
-import com.verapi.portal.common.Constants;
 import com.verapi.portal.oapi.exception.InternalServerError500Exception;
+import com.verapi.portal.service.ApiFilterQuery;
 import com.verapi.portal.service.idam.SubjectMembershipService;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -24,13 +24,8 @@ import io.vertx.reactivex.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
-import java.util.Objects;
-
-import static com.verapi.portal.common.Util.encodeFileToBase64Binary;
 
 @AbyssApiController(apiSpec = "/openapi/SubjectMembership.yaml")
 public class SubjectMembershipApiController extends AbstractApiController {
@@ -147,5 +142,25 @@ public class SubjectMembershipApiController extends AbstractApiController {
             throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
         }
     }
+
+    @AbyssApiOperationHandler
+    public void getMembershipsOfSubject(RoutingContext routingContext) {
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters"); //TODO: Lazım mı?
+
+        try {
+            getEntities(routingContext,
+                    SubjectMembershipService.class,
+                    null,
+                    new ApiFilterQuery()
+                            .setFilterQuery(SubjectMembershipService.FILTER_BY_SUBJECT)
+                            .setFilterQueryParams(new JsonArray().add(routingContext.pathParam("uuid"))));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
 
 }
--- src/main/java/com/verapi/portal/service/idam/SubjectMembershipService.java
+++ src/main/java/com/verapi/portal/service/idam/SubjectMembershipService.java
@@ -251,6 +251,8 @@ public class SubjectMembershipService extends AbstractService<UpdateResult> {
 
     private static final String SQL_CONDITION_NAME_LIKE = "";
 
+    private static final String SQL_CONDITION_SUBJECT_IS = "subjectid = CAST(? AS uuid)\n";
+
     private static final String SQL_ORDERBY_NAME = "order by id\n";
 
     private static final String SQL_CONDITION_ONLY_NOTDELETED = "isdeleted=false\n";
@@ -268,6 +270,8 @@ public class SubjectMembershipService extends AbstractService<UpdateResult> {
     private static final String SQL_DELETE_BY_UUID = SQL_DELETE_ALL + SQL_AND + SQL_CONDITION_UUID_IS;
 
     private static final String SQL_UPDATE_BY_UUID = SQL_UPDATE + SQL_WHERE + SQL_CONDITION_UUID_IS;
+
+    public static final String FILTER_BY_SUBJECT = SQL_SELECT + SQL_WHERE + SQL_CONDITION_SUBJECT_IS;
 
     private static final ApiFilterQuery.APIFilter apiFilter = new ApiFilterQuery.APIFilter(SQL_CONDITION_NAME_IS, SQL_CONDITION_NAME_LIKE);
 
