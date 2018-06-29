34ecaf3e8c394cc0204e91ed3e14f081c64db8b1
halil.ozkan <halil.ozkan@verapi.com>
2018-06-29 20:14:24 +0300

    [*] ABYSSP-200 #in-review #assign ilkiz.kiziltas #comment /oapi/api-licenses and /oapi/api-licenses/{uuid} api path developed with get, post, put and delete methods

---
+++ src/main/java/com/verapi/portal/service/idam/ApiLicenseService.java
@@ -0,0 +1,274 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 6 2018
+ *
+ */
+
+package com.verapi.portal.service.idam;
+
+import com.verapi.portal.common.AbyssJDBCService;
+import com.verapi.portal.oapi.CompositeResult;
+import com.verapi.portal.oapi.schema.ApiSchemaError;
+import com.verapi.portal.service.AbstractService;
+import com.verapi.portal.service.ApiFilterQuery;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.UpdateResult;
+import io.vertx.reactivex.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class ApiLicenseService extends AbstractService<UpdateResult> {
+    private static final Logger logger = LoggerFactory.getLogger(ApiLicenseService.class);
+
+    public ApiLicenseService(Vertx vertx, AbyssJDBCService abyssJDBCService) {
+        super(vertx, abyssJDBCService);
+    }
+
+    public ApiLicenseService(Vertx vertx) {
+        super(vertx);
+    }
+
+    public Single<List<JsonObject>> insertAll(JsonArray insertRecords) {
+        logger.trace("---insertAll invoked");
+        Observable<Object> insertParamsObservable = Observable.fromIterable(insertRecords);
+        return insertParamsObservable
+                .flatMap(o -> Observable.just((JsonObject) o))
+                .flatMap(o -> {
+                    JsonObject jsonObj = (JsonObject) o;
+                    JsonArray insertParam = new JsonArray()
+                            .add(jsonObj.getString("organizationid"))
+                            .add(jsonObj.getString("crudsubjectid"))
+                            .add(jsonObj.getString("apiid"))
+                            .add(jsonObj.getString("licenseid"));
+                    return insert(insertParam, SQL_INSERT).toObservable();
+                })
+                .flatMap(insertResult -> {
+                    if (insertResult.getThrowable() == null) {
+                        return findById(insertResult.getUpdateResult().getKeys().getInteger(0), SQL_FIND_BY_ID)
+                                .onErrorResumeNext(ex -> {
+                                    insertResult.setThrowable(ex);
+                                    return Single.just(insertResult.getResultSet()); //TODO: insertResult.throwable kayıp mı?
+                                })
+                                .flatMap(resultSet -> Single.just(insertResult.setResultSet(resultSet)))
+                                .toObservable();
+                    } else {
+                        return Observable.just(insertResult);
+                    }
+                })
+                .flatMap(result -> {
+                    JsonObject recordStatus = new JsonObject();
+                    if (result.getThrowable() != null) {
+                        logger.trace("insertAll>> insert/find exception {}", result.getThrowable());
+                        logger.error(result.getThrowable().getLocalizedMessage());
+                        logger.error(Arrays.toString(result.getThrowable().getStackTrace()));
+                        recordStatus
+                                .put("uuid", "0")
+                                .put("status", HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                                .put("response", new JsonObject())
+                                .put("error", new ApiSchemaError()
+                                        .setUsermessage(result.getThrowable().getLocalizedMessage())
+                                        .setCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                                        .setInternalmessage(Arrays.toString(result.getThrowable().getStackTrace()))
+                                        .toJson());
+                    } else {
+                        logger.trace("insertAll>> insert getKeys {}", result.getUpdateResult().getKeys().encodePrettily());
+                        JsonArray arr = new JsonArray();
+                        result.getResultSet().getRows().forEach(arr::add);
+                        recordStatus
+                                .put("uuid", result.getResultSet().getRows().get(0).getString("uuid"))
+                                .put("status", HttpResponseStatus.CREATED.code())
+                                .put("response", arr.getJsonObject(0))
+                                .put("error", new ApiSchemaError().toJson());
+                    }
+                    return Observable.just(recordStatus);
+                })
+                .toList();
+    }
+
+    public Single<CompositeResult> update(UUID uuid, JsonObject updateRecord) {
+        JsonArray updateParams = new JsonArray()
+                .add(updateRecord.getString("organizationid"))
+                .add(updateRecord.getString("crudsubjectid"))
+                .add(updateRecord.getString("apiid"))
+                .add(updateRecord.getString("licenseid"))
+                .add(uuid.toString());
+        return update(updateParams, SQL_UPDATE_BY_UUID);
+    }
+
+    public Single<List<JsonObject>> updateAll(JsonObject updateRecords) {
+        JsonArray jsonArray = new JsonArray();
+        updateRecords.forEach(updateRow -> {
+            jsonArray.add(new JsonObject(updateRow.getValue().toString())
+                    .put("uuid", updateRow.getKey()));
+        });
+        Observable<Object> updateParamsObservable = Observable.fromIterable(jsonArray);
+        return updateParamsObservable
+                .flatMap(o -> {
+                    JsonObject jsonObj = (JsonObject) o;
+                    JsonArray updateParam = new JsonArray()
+                            .add(jsonObj.getString("organizationid"))
+                            .add(jsonObj.getString("crudsubjectid"))
+                            .add(jsonObj.getString("apiid"))
+                            .add(jsonObj.getString("licenseid"))
+                            .add(jsonObj.getString("uuid"));
+                    return update(updateParam, SQL_UPDATE_BY_UUID).toObservable();
+                })
+                .flatMap(updateResult -> {
+                    if (updateResult.getThrowable() == null) {
+                        return findById(updateResult.getUpdateResult().getKeys().getInteger(0), SQL_FIND_BY_ID)
+                                .onErrorResumeNext(ex -> {
+                                    updateResult.setThrowable(ex);
+                                    return Single.just(updateResult.getResultSet()); //TODO: updateResult.throwable kayıp mı?
+                                })
+                                .flatMap(resultSet -> Single.just(updateResult.setResultSet(resultSet)))
+                                .toObservable();
+                    } else {
+                        return Observable.just(updateResult);
+                    }
+                })
+                .flatMap(result -> {
+                    JsonObject recordStatus = new JsonObject();
+                    if (result.getThrowable() != null) {
+                        logger.trace("updateAll>> update/find exception {}", result.getThrowable());
+                        logger.error(result.getThrowable().getLocalizedMessage());
+                        logger.error(Arrays.toString(result.getThrowable().getStackTrace()));
+                        recordStatus
+                                .put("uuid", "0")
+                                .put("status", HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                                .put("response", new JsonObject())
+                                .put("error", new ApiSchemaError()
+                                        .setUsermessage(result.getThrowable().getLocalizedMessage())
+                                        .setCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                                        .setInternalmessage(Arrays.toString(result.getThrowable().getStackTrace()))
+                                        .toJson());
+                    } else {
+                        logger.trace("updateAll>> update getKeys {}", result.getUpdateResult().getKeys().encodePrettily());
+                        JsonArray arr = new JsonArray();
+                        result.getResultSet().getRows().forEach(arr::add);
+                        recordStatus
+                                .put("uuid", result.getResultSet().getRows().get(0).getString("uuid"))
+                                .put("status", HttpResponseStatus.CREATED.code())
+                                .put("response", arr.getJsonObject(0))
+                                .put("error", new ApiSchemaError().toJson());
+                    }
+                    return Observable.just(recordStatus);
+                })
+                .toList();
+    }
+
+    public Single<CompositeResult> delete(UUID uuid) {
+        return delete(uuid, SQL_DELETE_BY_UUID);
+    }
+
+    public Single<CompositeResult> deleteAll() {
+        return deleteAll(SQL_DELETE_ALL);
+    }
+
+    public Single<CompositeResult> deleteAll(ApiFilterQuery apiFilterQuery) {
+        ApiFilterQuery sqlDeleteAllQuery = new ApiFilterQuery().setFilterQuery(SQL_DELETE_ALL).addFilterQuery(apiFilterQuery.getFilterQuery());
+        return deleteAll(sqlDeleteAllQuery.getFilterQuery());
+    }
+
+    public Single<ResultSet> findById(long id) {
+        return findById(id, SQL_FIND_BY_ID);
+    }
+
+    public Single<ResultSet> findById(UUID uuid) {
+        return findById(uuid, SQL_FIND_BY_UUID);
+    }
+
+    public Single<ResultSet> findByName(String name) {
+        return findByName(name, SQL_FIND_BY_NAME);
+    }
+
+    public Single<ResultSet> findLikeName(String name) {
+        return findLikeName(name, SQL_FIND_LIKE_NAME);
+    }
+
+    public Single<ResultSet> findAll() {
+        return findAll(SQL_SELECT);
+    }
+
+    public Single<ResultSet> findAll(ApiFilterQuery apiFilterQuery) {
+        return filter(apiFilterQuery);
+    }
+
+    public ApiFilterQuery.APIFilter getAPIFilter() {
+        return apiFilter;
+    }
+
+    private static final String SQL_INSERT = "insert into api_license (organizationid, crudsubjectid, apiid, licenseid)\n" +
+            "values (CAST(? AS uuid), CAST(? AS uuid), CAST(? AS uuid), CAST(? AS uuid))";
+
+    private static final String SQL_DELETE = "update api_license\n" +
+            "set\n" +
+            "  deleted     = now()\n" +
+            "  , isdeleted = true\n";
+
+    private static final String SQL_SELECT = "select\n" +
+            "  uuid,\n" +
+            "  organizationid,\n" +
+            "  created,\n" +
+            "  updated,\n" +
+            "  deleted,\n" +
+            "  isdeleted,\n" +
+            "  crudsubjectid,\n" +
+            "  apiid,\n" +
+            "  licenseid\n" +
+            "from api_license\n";
+
+    private static final String SQL_UPDATE = "UPDATE api_license\n" +
+            "SET\n" +
+            "  organizationid      = CAST(? AS uuid)\n" +
+            "  , updated               = now()\n" +
+            "  , crudsubjectid      = CAST(? AS uuid)\n" +
+            "  , apiid      = CAST(? AS uuid)\n" +
+            "  , licenseid      = CAST(? AS uuid)\n";
+
+    private static final String SQL_AND = "and\n";
+
+    private static final String SQL_WHERE = "where\n";
+
+    private static final String SQL_CONDITION_ID_IS = "id = ?\n";
+
+    private static final String SQL_CONDITION_UUID_IS = "uuid = CAST(? AS uuid)\n";
+
+    private static final String SQL_CONDITION_NAME_IS = "lower(name) = lower(?)\n";
+
+    private static final String SQL_CONDITION_NAME_LIKE = "lower(name) like lower(?)\n";
+
+    private static final String SQL_ORDERBY_NAME = "order by name\n";
+
+    private static final String SQL_CONDITION_ONLY_NOTDELETED = "isdeleted=false\n";
+
+    private static final String SQL_FIND_BY_ID = SQL_SELECT + SQL_WHERE + SQL_CONDITION_ID_IS;
+
+    private static final String SQL_FIND_BY_UUID = SQL_SELECT + SQL_WHERE + SQL_CONDITION_UUID_IS;
+
+    private static final String SQL_FIND_BY_NAME = SQL_SELECT + SQL_WHERE + SQL_CONDITION_NAME_IS;
+
+    private static final String SQL_FIND_LIKE_NAME = SQL_SELECT + SQL_WHERE + SQL_CONDITION_NAME_LIKE;
+
+    private static final String SQL_DELETE_ALL = SQL_DELETE + SQL_WHERE + SQL_CONDITION_ONLY_NOTDELETED;
+
+    private static final String SQL_DELETE_BY_UUID = SQL_DELETE_ALL + SQL_AND + SQL_CONDITION_UUID_IS;
+
+    private static final String SQL_UPDATE_BY_UUID = SQL_UPDATE + SQL_WHERE + SQL_CONDITION_UUID_IS;
+
+    private static final ApiFilterQuery.APIFilter apiFilter = new ApiFilterQuery.APIFilter(SQL_CONDITION_NAME_IS, SQL_CONDITION_NAME_LIKE);
+
+}
