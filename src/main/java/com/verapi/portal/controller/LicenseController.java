c72a180549e4486b4cebc3acdfbd30028dfbd1d8
halil.ozkan <halil.ozkan@verapi.com>
2018-07-02 19:01:12 +0300

    [*] ABYSSP-56 #comment license controller developed

---
+++ src/main/java/com/verapi/portal/controller/LicenseController.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 7 2018
+ *
+ */
+
+package com.verapi.portal.controller;
+
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AbyssController(routePathGET = "my-licenses", routePathPOST = "", htmlTemplateFile = "my-licenses.html")
+public class LicenseController extends PortalAbstractController {
+    private static Logger logger = LoggerFactory.getLogger(LicenseController.class);
+
+    public LicenseController(JDBCAuth authProvider, JDBCClient jdbcClient) {
+        super(authProvider, jdbcClient);
+    }
+
+    @Override
+    public void defaultGetHandler(RoutingContext routingContext) {
+        logger.trace("LicenseController.defaultGetHandler invoked...");
+        renderTemplate(routingContext, getClass().getAnnotation(AbyssController.class).htmlTemplateFile());
+    }
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        logger.trace("LicenseController.handle invoked...");
+    }
+
+}
