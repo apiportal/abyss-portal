5ddd03e8343f7fb97e8bd0cccf305223c7c6aa52
halil.ozkan <halil.ozkan@verapi.com>
2018-07-06 18:44:15 +0300

    [*] ABYSSP-218 #in-review #assign ilkiz.kiziltas #comment /subject-organizations, /subject-organizations/{uuid} and /subject-organizations/subject/{uuid} api paths developed with get, post, put and delete methods

---
+++ src/main/java/com/verapi/portal/oapi/SubjectOrganizationApiController.java
@@ -0,0 +1,166 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 7 2018
+ *
+ */
+
+package com.verapi.portal.oapi;
+
+import com.verapi.portal.oapi.exception.InternalServerError500Exception;
+import com.verapi.portal.service.ApiFilterQuery;
+import com.verapi.portal.service.idam.SubjectOrganizationService;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.api.RequestParameters;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+@AbyssApiController(apiSpec = "/openapi/SubjectOrganization.yaml")
+public class SubjectOrganizationApiController extends AbstractApiController {
+    private static final Logger logger = LoggerFactory.getLogger(SubjectOrganizationApiController.class);
+
+    /**
+     * API verticle creates new API Controller instance via this constructor
+     *
+     * @param vertx        Vertx content
+     * @param router       Vertx router
+     * @param authProvider JDBC Auth provider
+     */
+    public SubjectOrganizationApiController(Vertx vertx, Router router, JDBCAuth authProvider) {
+        super(vertx, router, authProvider);
+    }
+
+    @AbyssApiOperationHandler
+    public void getSubjectOrganizations(RoutingContext routingContext) {
+        try {
+            getEntities(routingContext, SubjectOrganizationService.class);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void addSubjectOrganizations(RoutingContext routingContext) {
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters");
+
+        // We get an user JSON array validated by Vert.x Open API validator
+        JsonArray requestBody = requestParameters.body().getJsonArray();
+
+        try {
+            addEntities(routingContext, SubjectOrganizationService.class, requestBody);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void updateSubjectOrganizations(RoutingContext routingContext) {
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters");
+
+        // We get an user JSON object validated by Vert.x Open API validator
+        JsonObject requestBody = requestParameters.body().getJsonObject();
+
+        //now it is time to update entities
+        try {
+            updateEntities(routingContext, SubjectOrganizationService.class, requestBody);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void deleteSubjectOrganizations(RoutingContext routingContext) {
+        try {
+            deleteEntities(routingContext, SubjectOrganizationService.class);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void getSubjectOrganization(RoutingContext routingContext) {
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters");
+
+        try {
+            getEntity(routingContext, SubjectOrganizationService.class);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void updateSubjectOrganization(RoutingContext routingContext) {
+
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters");
+
+        // We get an user JSON object validated by Vert.x Open API validator
+        JsonObject requestBody = requestParameters.body().getJsonObject();
+
+        try {
+            updateEntity(routingContext, SubjectOrganizationService.class, requestBody);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void deleteSubjectOrganization(RoutingContext routingContext) {
+
+        try {
+            deleteEntity(routingContext, SubjectOrganizationService.class);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+    @AbyssApiOperationHandler
+    public void getOrganizationsOfSubject(RoutingContext routingContext) {
+        // Get the parsed parameters
+        RequestParameters requestParameters = routingContext.get("parsedParameters"); //TODO: Lazım mı?
+
+        try {
+            getEntities(routingContext,
+                    SubjectOrganizationService.class,
+                    null,
+                    new ApiFilterQuery()
+                            .setFilterQuery(SubjectOrganizationService.FILTER_BY_SUBJECT)
+                            .setFilterQueryParams(new JsonArray().add(routingContext.pathParam("uuid"))));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getLocalizedMessage());
+            logger.error(Arrays.toString(e.getStackTrace()));
+            throwApiException(routingContext, InternalServerError500Exception.class, e.getLocalizedMessage());
+        }
+    }
+
+
+}
