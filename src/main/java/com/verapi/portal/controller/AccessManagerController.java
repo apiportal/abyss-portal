394ca2af31a71f735d67810518788402f5d1ec97
halil.ozkan <halil.ozkan@verapi.com>
2018-07-06 20:52:15 +0300

    [+] ABYSSP-238 #comment access-manager and identity-manager controllers developed, webroot merged

---
+++ src/main/java/com/verapi/portal/controller/AccessManagerController.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 7 2018
+ *
+ */
+
+package com.verapi.portal.controller;
+
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AbyssController(routePathGET = "access-managers", routePathPOST = "access-managers", htmlTemplateFile = "access-managers.html")
+public class AccessManagerController extends PortalAbstractController {
+    private static Logger logger = LoggerFactory.getLogger(AccessManagerController.class);
+
+    public AccessManagerController(JDBCAuth authProvider, JDBCClient jdbcClient) {
+        super(authProvider, jdbcClient);
+    }
+
+    @Override
+    public void defaultGetHandler(RoutingContext routingContext) {
+        logger.trace("AccessManagerController.defaultGetHandler invoked...");
+        renderTemplate(routingContext, getClass().getAnnotation(AbyssController.class).htmlTemplateFile());
+    }
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        logger.trace("AccessManagerController.handle invoked...");
+    }
+
+}
---
+++ src/main/java/com/verapi/portal/controller/IdentityManagerController.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 7 2018
+ *
+ */
+
+package com.verapi.portal.controller;
+
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AbyssController(routePathGET = "identity-managers", routePathPOST = "identity-managers", htmlTemplateFile = "identity-managers.html")
+public class IdentityManagerController extends PortalAbstractController {
+    private static Logger logger = LoggerFactory.getLogger(IdentityManagerController.class);
+
+    public IdentityManagerController(JDBCAuth authProvider, JDBCClient jdbcClient) {
+        super(authProvider, jdbcClient);
+    }
+
+    @Override
+    public void defaultGetHandler(RoutingContext routingContext) {
+        logger.trace("IdentityManagerController.defaultGetHandler invoked...");
+        renderTemplate(routingContext, getClass().getAnnotation(AbyssController.class).htmlTemplateFile());
+    }
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        logger.trace("IdentityManagerController.handle invoked...");
+    }
+
+}
