3473144c0333055c26c8e1de8403c290a125222a
faik.saglar <faik.saglar@verapi.com>
2018-07-16 14:34:59 +0300

    [*] OpenApiParser error logging extented to include API Title

--- src/main/java/com/verapi/portal/common/OpenAPIUtil.java
+++ src/main/java/com/verapi/portal/common/OpenAPIUtil.java
@@ -47,10 +47,10 @@ public class OpenAPIUtil {
                 return Single.just(swaggerParseResult);
             } else {
                 if (swaggerParseResult.getMessages().size() == 1 && swaggerParseResult.getMessages().get(0).matches("unable to read location")) {
-                    logger.error("openAPIParser error | {}", swaggerParseResult.getMessages());
+                    logger.error("openAPIParser error for: {}| {}", data.substring(1,40), swaggerParseResult.getMessages());
                     return Single.error(RouterFactoryException.createSpecNotExistsException(""));
                 } else {
-                    logger.error("openAPIParser error | {}", swaggerParseResult.getMessages());
+                    logger.error("openAPIParser error for: {}| {}", data.substring(1,40), swaggerParseResult.getMessages());
                     return Single.error(RouterFactoryException.createSpecInvalidException(StringUtils.join(swaggerParseResult.getMessages(), ", ")));
                 }
             }
