0d95f85c6aa57510926ef45b69c27e549f62a3a1
faik.saglar <faik.saglar@verapi.com>
2018-07-26 10:38:06 +0300

    [+] Permission controller developed.

---
+++ src/main/java/com/verapi/portal/controller/PermissionController.java
@@ -0,0 +1,39 @@
+/*
+ *
+ *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  *
+ *  *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  Proprietary and confidential
+ *  *
+ *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 7 2018
+ *
+ */
+
+package com.verapi.portal.controller;
+
+import io.vertx.reactivex.ext.auth.jdbc.JDBCAuth;
+import io.vertx.reactivex.ext.jdbc.JDBCClient;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AbyssController(routePathGET = "permissions", routePathPOST = "", htmlTemplateFile = "permissions.html")
+public class PermissionController extends PortalAbstractController {
+    private static Logger logger = LoggerFactory.getLogger(PermissionController.class);
+
+    public PermissionController(JDBCAuth authProvider, JDBCClient jdbcClient) {
+        super(authProvider, jdbcClient);
+    }
+
+    @Override
+    public void defaultGetHandler(RoutingContext routingContext) {
+        logger.trace("PermissionController.defaultGetHandler invoked...");
+        renderTemplate(routingContext, getClass().getAnnotation(AbyssController.class).htmlTemplateFile());
+    }
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        logger.trace("PermissionController.handle invoked...");
+    }
+
+}
